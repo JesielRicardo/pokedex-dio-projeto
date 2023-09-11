489c2cbec6310a74afa2ca71e6ef5eed75c52e2d
Jesiel Ricardo <ricardospeed2@gmail.com>
2023-09-10 21:42:00 -0300

    Mudanças da pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,6 @@
-
-
 const offset = 0
 const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}0&limit=${limit}`
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset * limit}&limit=${limit}`;
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -14,18 +12,15 @@ function convertPokemonToLi(pokemon) {
                     <ol class="types">
                         <li class="type">grass</li>                        
                         <li class="type">poison</li>
-
                     </ol>
-
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt=">
-
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
                 </div>
-                
             </li> 
     `
 }
 
-const pokemonList = document.getElementById("pokemonList");
+const pokemonList = document.getElementById('pokemonList');
+
 fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
